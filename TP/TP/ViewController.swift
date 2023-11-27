a3ac3baacbc74ead4e9cd5c139f6efc775923ea4
abdousabi <117770202+abdousabi@users.noreply.github.com>
2023-11-27 10:04:31 +0100

    Add files via upload

---
+++ TP/TP/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TP
+//
+//  Created by esirem on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+ 
+    @IBOutlet weak var tasktableview: UITableView!
+    var mytasks = [Todo]()
+    
+   
+    
+    
+    
+    
+    
+}
+
---
+++ TP/TP/todo.swift
@@ -0,0 +1,20 @@
+//
+//  todo.swift
+//  TP
+//
+//  Created by esirem on 27/11/2023.
+//
+
+import Foundation
+class Todo{
+    var name: String
+    var description: String
+    init (name: String, description: String){
+        
+        self.name = name
+        self.description = description
+        
+        
+        
+    }
+}
